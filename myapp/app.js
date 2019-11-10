463c52fb6d8138b3fe3c215e1c2e0282a9d2efb7
kamimi01 <awakaru0122@gmail.com>
2019-11-10 13:50:06 +0900

    hello worldを表示する

--- myapp/app.js
+++ myapp/app.js
@@ -1,4 +1,15 @@
 // expressのモジュールの読み込み
+// appとすると、expressを使ってオブジェクトを作ってくれる
 var express = require('express'),
     app = express();
 
+// express4以降、この文言は不要のため削除
+// app.use(app.router);
+
+// スラッシュでアクセスしてきたら、次のことをしなさいよと言う命令
+app.get('/', function(req,res) {
+    res.send('hello world');
+});
+
+app.listen(3000);
+console.log("server starting...");
