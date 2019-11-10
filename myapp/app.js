047f6dbb9225685d82e723c56067662d0fc283e5
kamimi01 <awakaru0122@gmail.com>
2019-11-10 14:10:38 +0900

    静的ファイルの中身の送信

--- myapp/app.js
+++ myapp/app.js
@@ -5,27 +5,16 @@ var express = require('express'),
 
 // express4以降、この文言は不要のため削除
 // app.use(app.router);
+// 静的なファイルは、/publicから読み込みしてほしい、とする
+app.use(express.static(__dirname + '/public'))
 
 // スラッシュでアクセスしてきたら、次のことをしなさいよと言う命令
 // usersの後の文言を処理の中に表示させる（URLの一部を取得することができる）
-app.get('/users/:name?', function(req,res) {
-    if (req.params.name) {
-        res.send('hello, ' + req.params.name);
-    } else {
-        res.send('hello, nobody')
-    }
+// ファイルを読み出したい場合には、sendFileを使用する
+// わざわざ指定するのは面倒なので、express.staticを使用する
+app.get('/hello.txt', function(req,res) {
+    res.sendFile(__dirname + '/public/hello.txt');
 });
-
-// パラメータに正規表現で制限をかけることができる
-app.get('/items/:id([0-9]+)', function(req,res) {
-    res.send('item no: ' + req.params.id);
-});
-/*
-//スラッシュを使った、ルーティングの設定をすると、ルーターが読み込んでくれる
-app.get('/about', function(req,res) {
-    res.send('about this page!');
-});
-*/
 
 app.listen(3000);
 console.log("server starting...");
