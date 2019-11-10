9410780d24e3d40bfd85eeaf9ce5671a2f75b2b2
kamimi01 <awakaru0122@gmail.com>
2019-11-10 17:03:00 +0900

    ルーティングの設定の追加

--- blog/app.js
+++ blog/app.js
@@ -15,13 +15,26 @@ app.set('view engine', 'ejs');
 // app.useで読み込まれるものを、middlewareと呼ぶ（expressにおいて）
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+// ブラウザは、getとpostにしか対応していないので、putとdeleteにも対応できるようにするmiddlewareが必要
+app.use(express.methodOverride());
 app.use(logger('dev'));
 
 // routing（いろんな関数を書くのだが、見通しが悪くなるので、別ファイルに書く）
+// 記事一覧
+app.get('/', post.index);
+// 記事の新規作成のフォーム表示
+app.get('/posts/new', post.new);
+// 記事の新規作成した記事の表示
+app.post('/posts/create', post.create);
+// 記事の詳細画面
+app.get('/posts/:id', post.show);
+// 記事の更新フォーム
+app.get('/posts/:id/edit', post.edit);
+// 記事の更新を実施する
+app.put('/posts/:id', post.update);
+// 記事の削除
+app.delete('/posts/:id', post.destroy);
 
-app.get('/new', function(req, res) {
-    res.render('new');
-});
 
 app.listen(3000);
 console.log("server starting...");
