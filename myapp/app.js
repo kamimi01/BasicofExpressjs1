2a17461734928c48fed527e6a47a503743b3cc5b
kamimi01 <awakaru0122@gmail.com>
2019-11-10 14:01:51 +0900

    urlの中身を処理に反映する

--- myapp/app.js
+++ myapp/app.js
@@ -7,9 +7,25 @@ var express = require('express'),
 // app.use(app.router);
 
 // スラッシュでアクセスしてきたら、次のことをしなさいよと言う命令
-app.get('/', function(req,res) {
-    res.send('hello world');
+// usersの後の文言を処理の中に表示させる（URLの一部を取得することができる）
+app.get('/users/:name?', function(req,res) {
+    if (req.params.name) {
+        res.send('hello, ' + req.params.name);
+    } else {
+        res.send('hello, nobody')
+    }
 });
+
+// パラメータに正規表現で制限をかけることができる
+app.get('/items/:id([0-9]+)', function(req,res) {
+    res.send('item no: ' + req.params.id);
+});
+/*
+//スラッシュを使った、ルーティングの設定をすると、ルーターが読み込んでくれる
+app.get('/about', function(req,res) {
+    res.send('about this page!');
+});
+*/
 
 app.listen(3000);
 console.log("server starting...");
